2802daeec40b7e1836aeab864f2bd2e76d45cd0b
Tanishka71 <86073425+Tanishka71@users.noreply.github.com>
2023-11-29 20:30:22 +0530

    use case 9

--- LL.java
+++ LL.java
@@ -1,14 +1,14 @@
 package com.bridgelabz.main.linkedlist;
 //@desc:main class
 public class LL {
-
 	public static void main(String []args) {
 		CustomLL linkedList=new CustomLL();
 	     linkedList.addFirst(70);
-	     linkedList.addFirst(30);
+	     linkedList.addFirst(40);
+	     linkedList.addFirst(40);
 	     linkedList.addFirst(56);
-         int pos=linkedList.find(30);
-         System.out.println("it is present at: "+pos);
-		linkedList.addPos(40,pos+1);
+         int pos=linkedList.find(40);
+         linkedlist.delPos(pos);
+         linkedlist.size();
 }
 }
