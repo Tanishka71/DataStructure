9de8adc238532200161ef4fcf13e54f30db460f4
Tanishka71 <86073425+Tanishka71@users.noreply.github.com>
2023-11-29 20:37:00 +0530

    UC 10

--- LL.java
+++ LL.java
@@ -4,11 +4,15 @@ public class LL {
 
 
 	public static void main(String []args) {
-		CustomLL linkedList=new CustomLL();
-	     linkedList.addFirst(70);
-	     linkedList.addFirst(30);
-	     linkedList.addFirst(56);
-         linkedList.delLast();
-         linkedList.display();
+ SortedLinkedList<Integer> sortedList = new SortedLinkedList<>();
+	        
+	        // Adding elements in ascending order
+	        sortedList.add(56);
+	        sortedList.add(30);
+	        sortedList.add(40);
+	        sortedList.add(70);
+
+	        // Displaying the final sequence
+	        sortedList.display();
 }
 }
