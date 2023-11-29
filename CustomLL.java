6103c5a6f3a6857d091a9e99c3eae7d2376e5cdf
Tanishka71 <86073425+Tanishka71@users.noreply.github.com>
2023-11-29 20:36:25 +0530

    UC 10

--- CustomLL.java
+++ CustomLL.java
@@ -8,26 +8,56 @@ class Node{
 			next=null;
 		}
 }
+//@DESC: USIING GENERIC CLASS AND COMPARABLE
+class SortedLinkedList<T extends Comparable<T>> {
+    private Node<T> head;
+    public static class Node<T> {
+        T data;
+        Node<T> next;
 
-//@desc:custom linked list class that has all the functions
-public class CustomLL {
-   Node head=null;
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
 /*
 * @desc:add elements in linked list
 * @params:Integer data
 * @return:none
 */
- public void addFirst(int element) {
- Node newNode=new Node(element);
- if(head ==null) {
-	   head=newNode;
- }
- else {
-	   newNode.next=head;
-	   head=newNode;
- }
- 
+    public void add(T data) {
+        Node<T> newNode = new Node<>(data);
+
+        if (head == null || data.compareTo(head.data) < 0) {
+            newNode.next = head;
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.next != null && data.compareTo(current.next.data) > 0) {
+                current = current.next;
+            }
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+    }
+   
+  /*
+  * @desc:display the elements of the LL
+  * @params:none
+  * @return:none
+  */
+    public void display() {
+        Node<T> current = head;
+        while (current.next != null) {
+            System.out.print(current.data + " -->");
+            current = current.next;
+        }
+        System.out.println(current.data);
+    }
 }
+
+
+
  
  /*
  * @desc:DEL last elements in linked list
