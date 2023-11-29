82bea7e6f528c12424d76935d2e0a4cfa8df3f1d
Tanishka Jain <tanishkajain1508@gmail.com>
2023-11-29 19:31:59 +0530

    ADDED:USE CASE 1

---
+++ CustomLL.java
@@ -0,0 +1,48 @@
+package com.bridgelabz.main.linkedlist;
+
+class Node{
+	int data;
+	Node next;
+		Node(int data){
+			this.data=data;
+			next=null;
+		}
+}
+
+//@desc:custom linked list class that has all the functions
+public class CustomLL {
+   Node head=null;
+   
+/*
+* @desc:add elements in linked list
+* @params:Integer data
+* @return:none
+*/
+  public  void add(int element) {
+	   Node newNode=new Node(element);
+	   if(head ==null) {
+		   head=newNode;
+	   }
+	   else {
+		   newNode.next=head;
+		   head=newNode;
+	   }
+	   
+   }
+   
+  /*
+  * @desc:display the elements of the LL
+  * @params:none
+  * @return:none
+  */
+ public  void display() {
+	   Node curr=head;
+	   while(curr.next != null) {
+		   System.out.print(curr.data+" --> ");
+		   curr=curr.next;
+	   }
+	   System.out.println(curr.data);
+   }
+}
+
+ 
---
+++ LL.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.main.linkedlist;
+
+public class LL {
+
+	public static void main(String []args) {
+		CustomLL linkedList=new CustomLL();
+	     linkedList.add(56);
+	     linkedList.add(30);
+	     linkedList.add(70);
+         linkedList.display();
+}
+}
