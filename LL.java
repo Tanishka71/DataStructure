c306a96807d1ab1d88b31d55ed1a620c8137e7cd
Tanishka71 <86073425+Tanishka71@users.noreply.github.com>
2023-11-29 20:06:08 +0530

    use case 3

--- LL.java
+++ LL.java
@@ -4,9 +4,9 @@ public class LL {
 
 	public static void main(String []args) {
 		CustomLL linkedList=new CustomLL();
-	     linkedList.addFirst(70);
-	     linkedList.addFirst(30);
-	     linkedList.addFirst(56);
+	     linkedList.addLast(70);
+	     linkedList.addLast(30);
+	     linkedList.addLast(56);
          linkedList.display();
 }
 }
