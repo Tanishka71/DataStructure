53f72bd0295b7d9cab1e2a3d10674b40649dbf23
Tanishka71 <86073425+Tanishka71@users.noreply.github.com>
2023-11-29 20:33:14 +0530

    use case 9

--- LLtest.java
+++ LLtest.java
@@ -19,4 +19,17 @@ class LLtest {
 				"Find method should give the position of the key");
 	    obj.addPos(40,3);
 	}
+
+	@Test	
+	void testCase2() {
+	     obj.addFirst(70);
+	     obj.addFirst(30);
+	     obj.addFirst(40);
+	     obj.addFirst(56);
+             int pos=obj.find(40);
+             obj.delPos(pos); 
+               assertEquals(3,obj.size(), 
+				"size methode gives the size of the linked list");
+
+}
 }
